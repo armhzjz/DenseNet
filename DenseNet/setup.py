05cfbcbbe9fd98f46fbf87289796070ad9a0623d
Armando Hernandez <j.armandohernandez.j@gmail.com>
2020-05-25 00:32:51 +0200

    densenet pypi package version 0.1.2

--- DenseNet/setup.py
+++ DenseNet/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="DenseNet-armhzjz",
-    version="0.1.1",
+    version="0.1.2",
     author="Armando Hernandez",
     author_email="armhzjz@pm.me",
     description="A DenseNet implementation",
